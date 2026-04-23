14d9d516110ce0db9fe5169254dde9a67521f9c1
SonuR12 <sonurai7011@gmail.com>
2026-04-23 09:33:33 +0530

    sitemap update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseUrl,
-      lastModified: new Date('2024-12-19'),
+      lastModified: new Date('2026-04-23'),
       changeFrequency: 'weekly',
       priority: 1,
     },
