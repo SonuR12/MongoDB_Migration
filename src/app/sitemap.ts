bd4fc521ae9cd6dc9582143dc6055df6f81c0d55
SonuR12 <sonurai7011@gmail.com>
2026-04-24 19:10:33 +0530

    sitemap fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://mongodbmigrate.vercel.app'
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL!
   
   return [
     {
